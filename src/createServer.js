829677820ec3625171c3c47da892bc93851b2012
mk <makarkarma21@gmail.com>
2024-12-12 19:57:59 +0200

    add task solution3

--- src/createServer.js
+++ src/createServer.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const { userRouter } = require('./routes/userRoute');
 const { expenseRouter } = require('./routes/expenseRoute');
 const userService = require('./services/userService');
-const expenseSrvice = require('./services/expensesService');
+const expenseService = require('./services/expensesService');
 
 function createServer() {
   const app = express();
 
   app.use(cors());
   userService.start();
-  expenseSrvice.start();
+  expenseService.start();
 
   app.use('/users', express.json(), userRouter);
   app.use('/expenses', express.json(), expenseRouter);
